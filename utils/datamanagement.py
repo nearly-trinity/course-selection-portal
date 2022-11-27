2fdcd876e717a26543594884537de6d06aec8982
nathan early <Nathan Early>
2022-11-27 12:02:49 -0600

    initial commit

---
+++ utils/datamanagement.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def cleanPrerequisites(df):
+    return
+
+def getRows(filename):
+    df = pd.read_excel(filename)
+    
+    return 
---
+++ utils/main.py
@@ -0,0 +1,10 @@
+from utils.datamanagement import *
+
+def main():
+    _filename = "..\data\courses.xlsx"
+    df = getRows(_filename)
+    
+    return
+
+if __name__ == "__main__":
+    main()
